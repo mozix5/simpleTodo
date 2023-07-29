b826ba6a94646bd0275394dd52d2e186c750a9a6
mozix5 <76263118+mozix5@users.noreply.github.com>
2023-07-29 23:50:31 +0530

    Update Home.jsx

--- todo-list/src/pages/Home.jsx
+++ todo-list/src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Leftbar from "../components/Leftbar";
 import { Outlet } from "react-router-dom";
 import Rightbar from "../components/Rightbar";
 import Header from "../components/Header";
-import { useDarkMode } from "../components/TodoContext";
+import { useDarkMode, useModal, useNote, useView } from "../components/TodoContext";
+import Todo from "../components/Todo";
 
 const Home = () => {
   const { darkMode } = useDarkMode();
+  const { getFilteredNote, searchQuery } = useNote();
+  const filteredTodos = getFilteredNote();
+  const {openModal}=useModal()
+  
+
+  // const { getFilteredNote } = useNote();
+
   return (
     <div className={`${darkMode ? "dark" : ""}`}>
       <div className="fixed z-10">
@@ -14,7 +22,27 @@ const Home = () => {
       </div>
       <div className=" bg-slate-200 min-h-screen mx-64 dark:bg-slate-900">
         <Header />
-        <Outlet />
+
+        {searchQuery.trim() === "" ? (
+          <Outlet />
+        ) : (
+          <div
+            className={`mt-4 grid grid-cols-3 gap-2 sm:gap-4 xl:gap-6 items-end mx-8`}
+          >
+           { filteredTodos.map((task, index) => (
+            <Todo
+              key={index}
+              id={index}
+              title={task.title}
+              desc={task.desc}
+              date={task.date}
+              completed={task.completed}
+              important={task.important}
+            />
+            ) 
+            )}
+          </div>
+        )}
       </div>
       <div className="fixed right-0 top-0 bottom-0">
         <Rightbar />
