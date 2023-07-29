5426d08fc7cb533429610ec54afda8938d937cf2
mozix5 <76263118+mozix5@users.noreply.github.com>
2023-07-29 23:49:11 +0530

    Update Header.jsx

--- todo-list/src/components/Header.jsx
+++ todo-list/src/components/Header.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiBell } from "react-icons/hi";
-import { useNote } from "./TodoContext";
+import { useModal, useNote } from "./TodoContext";
 const Header = () => {
-  // const [searchQuery, setSearchQuery] = useState("");
-
+  
+  const {  searchQuery, setSearchQuery,getFilteredNote} = useNote();
+  const {openModal}=useModal()
   const currentDate = new Date();
   const options = { year: "numeric", month: "short", day: "2-digit" };
   const formattedDate = currentDate.toLocaleDateString("en-US", options);
 
  
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
+ 
+  const handleSearch = () => {
+    getFilteredNote();
+  };
 
   return (
     <div className="py-5 dark:text-slate-400 px-8 flex justify-between items-center ">
@@ -18,15 +26,17 @@ const Header = () => {
         <input
           type="text"
           placeholder="Search task"
+          value={searchQuery}
+          onChange={handleSearchChange}
           className="py-3.5 px-3 rounded-lg w-80 dark:bg-slate-800"
           
         />
-        <BiSearch className="absolute top-4 text-xl right-4 text-slate-400" />
+        <BiSearch onClick={handleSearch} className="absolute top-4 text-xl right-4 text-slate-400 hover:cursor-pointer" />
       </div>
       <div className="flex-1 flex justify-center pl-6">{formattedDate}</div>
       <div className="flex-1 flex justify-end items-center gap-6">
         <HiBell className="text-2xl text-violet-600 dark:text-violet-800" />
-        <button className=" w-36 text-white py-3 rounded-lg bg-violet-600 hover:bg-violet-700 dark:bg-violet-800">
+        <button className=" w-36 text-white py-3 rounded-lg bg-violet-600 hover:bg-violet-700 dark:bg-violet-800" onClick={openModal}>
           Add new task
         </button>
       </div>
